01307c194ab2593708e938e0d6012bdb51881ae2
TMaglietta <36413885+TMaglietta@users.noreply.github.com>
2018-02-16 18:43:40 -0500

    Add files via upload

---
+++ 05/CommentsAndSlashes.java
@@ -0,0 +1,15 @@
+public class CommentsAndSlashes{
+	public static void main (String [] args){
+        // A comment, this is so you can read your program later.
+        // Anything after the // is ignored by Java.
+		System.out.println("I could have code like this."); //and the comment after is ignored
+		
+		//You can also comment to disable a line of code
+		//System.out.println("this won't run");
+		
+		System.out.println("this will run");
+		/*
+			you can also comment like this
+		*/
+	}
+}
