b8bdb09d8d0e70d4f37701f2980e2df98890cbaf
TMaglietta <36413885+TMaglietta@users.noreply.github.com>
2018-02-16 18:53:31 -0500

    Add files via upload

---
+++ 07/Initials.java
@@ -0,0 +1,11 @@
+public class Initials{
+	public static void main(String [] args){
+		System.out.println("TTTTTT M     M");
+		System.out.println("  TT   MM   MM");
+		System.out.println("  TT   M M M M");
+		System.out.println("  TT   M  M  M");
+		System.out.println("  TT   M     M");
+		System.out.println("  TT   M     M");
+		System.out.println("  TT   M     M");
+	}
+}
