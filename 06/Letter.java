6219573a29c7dfab31627c5a4827c33a6d092c6c
TMaglietta <36413885+TMaglietta@users.noreply.github.com>
2018-02-16 18:43:25 -0500

    Add files via upload

---
+++ 06/Letter.java
@@ -0,0 +1,15 @@
+public class Letter{
+	public static void main(String[] args){
+		System.out.println("+---------------------------------------------------------+");
+		System.out.println("|                                                    #### |");
+		System.out.println("|                                                    #### |");
+		System.out.println("|                                                    #### |");
+		System.out.println("|                                                         |");
+		System.out.println("|                                                         |");
+		System.out.println("|                              Not doxxed yet             |");
+		System.out.println("|                              0 Doxxed Street            |");
+		System.out.println("|                              City, PR D0X 1O1           |");
+		System.out.println("|                                                         |");
+		System.out.println("+---------------------------------------------------------+");
+	}
+}
