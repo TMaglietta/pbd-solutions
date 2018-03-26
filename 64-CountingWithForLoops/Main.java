fe6774ca3b8cff76d371d85239187c2dc9505bd5
TMaglietta <36413885+TMaglietta@users.noreply.github.com>
2018-03-26 10:04:51 -0400

    Update Main.java

--- 64-CountingWithForLoops/Main.java
+++ 64-CountingWithForLoops/Main.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Type in a message, and I'll display it five times.");
+        System.out.print("Message: ");
+        String message = in.nextLine();
+        
+        for(int i = 2; i <= 10; i+=2){
+            System.out.println(i + ". " + message);
+        }
+    }
+}
+/*
+    1.  i += 1 increases i by 1 after each iteration 
+*/
