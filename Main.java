52f2ad54d823c774518bd4a8f64504c061c9d329
TMaglietta <36413885+TMaglietta@users.noreply.github.com>
2018-03-29 10:48:18 -0400

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-class Main {
-    public static void main(String[] args) {
-        System.out.println("  x\t  y\n-------------");
-        for(float x = -10; x <= 10; x += 0.5){
-            System.out.println(x + "\t" + (x*x));
-        }
-    }
-}
